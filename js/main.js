f3bc3db84e2a0f935ae5e09f4207e5b7152d3bc2
Arhiiiip <rabokony@icloud.com>
2022-09-12 22:04:45 +0300

    Version 1

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
+let count_rows = 0;
+
+let nameTag = ['X', 'Y', 'R', 'CT', 'ET', 'Re'];
+
 $('form').on('submit', function (event) {
     event.preventDefault()
-    if (validate()) {
+    let x = $('#xArgument').val();
+    let y = $('#yArgument').val();
+    let r = $('#rArgument').val();
+    if (validate(x, y, r)) {
+        shoot(x, y, r);
         $.ajax({
             url: 'php/main.php',
             type: 'POST',
             cache: false,
-            dataType: "json",
             data:
                 "x_value=" + document.getElementById("xArgument").value +
                 "&y_value=" + document.getElementById("yArgument").value +
                 "&r_value=" + document.getElementById("rArgument").value,
         }).done(add_row)
             .fail(processError)
+    } else {
+        let errorRow  = document.getElementById('error');
+        errorRow.innerHTML = "Data not valid";
     }
 })
 
@@ -23,53 +33,73 @@ function processError(xhr, status, errorThrown) {
 }
 
 function add_row(data) {
-    let row = $('#historyTable');
-    for (let i in data) {
-        $('<td>').html(data[i]).appendTo(row);
+    let countTag = 0;
+    count_rows = count_rows + 1;
+    cleanError();
+    let start_row = document.getElementById('startTable');
+    let real_row = document.createElement('tr');
+    for (let i in data){
+        let info = document.createElement('td');
+        info.innerHTML = data[i];
+        real_row.appendChild(info);
+        let tag = nameTag[countTag] + count_rows;
+        add_storage(tag, data[i]);
+        countTag = countTag + 1;
     }
-    $(row).insertAfter($('table tr:last').eq(0));
+    start_row.after(real_row);
+}
+
+function add_storage(key, data){
+    sessionStorage.setItem(key, data);
 }
 
 function validate(x, y, r) {
-    if (x > -3 && x < 5 && y > -3 && y < 5 && r < 5 && r > 2) {
-        return true
-    } else {
-        return false
-    }
+    return +x > -3 && +x < 5 && +y > -3 && +y < 3 && +r < 5 && +r > 2;
 }
 
 
-window.onload = function () {
-    document.querySelector('.form').onchange = function () {
-        let x = $('#xArgument').val()
-        let y = $('#yArgument').val()
-        let r = $('#rArgument').val()
-        if (validate(x, y, r)) {
-            let dots = document.getElementById('dot')
-            if (dots == null) {
-            } else {
-                dots.remove()
-            }
-            let cr = +r;
-            let xc = +x * (150 / cr) + 200;
-            let yc = -(+y * (150 / cr)) + 200;
-            const shoot = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-            shoot.setAttribute('r', '4')
-            shoot.setAttribute('cx', String(xc))
-            shoot.setAttribute('cy', String(yc))
-            shoot.setAttribute('fill', 'black')
-            shoot.setAttribute('color', 'black')
-            shoot.setAttribute('id', 'dot')
-            $('.graph').html(appendChild(shoot))
+function shoot(x, y, r) {
+    if (validate(x, y, r)) {
+        let dots = document.getElementById('dot')
+        if (dots == null) {
         } else {
-            alert('Data isn\'t valid')
+            dots.remove()
         }
+        let cr = +r;
+        let xc = +x * (150 / cr) + 200;
+        let yc = -(+y * (150 / cr)) + 200;
+        const shoot = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+        shoot.setAttribute('r', '4');
+        shoot.setAttribute('cx', String(xc));
+        shoot.setAttribute('cy', String(yc));
+        shoot.setAttribute('fill', 'black');
+        shoot.setAttribute('color', 'black');
+        shoot.setAttribute('id', 'dot');
+        let pole = document.getElementById('graph');
+        pole.appendChild(shoot);
+    } else {
+        alert('okey')
     }
 }
-// function reset(){
-//     while (!(document.getElementById('dot').is(':empty'))) {
-//         let dots = document.getElementById('dot')
-//         dots.remove()
-//     }
-// }
 
+function cleanError(){
+    let errorRow = document.getElementById('error');
+    errorRow.innerHTML = '';
+
+}
+
+window.onload = function() {
+    let ssLen = (sessionStorage.length/6);
+    let start_row = document.getElementById('startTable');
+    while(ssLen > 0){
+        let real_row = document.createElement('tr');
+        for(let j in nameTag){
+            let key = nameTag[j] + ssLen;
+            let info = document.createElement('td');
+            info.innerHTML = sessionStorage.getItem(key);
+            real_row.appendChild(info);
+        }
+        start_row.after(real_row)
+        ssLen = ssLen - 1;
+    }
+}
